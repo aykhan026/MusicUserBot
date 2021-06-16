13d6885c119fb5dbbe6c2175941b6aa7bd5b17d2
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-06-16 20:13:59 +0400

    Add files via upload

--- ֆɦɨռɨɢǟʍɨ_ʀʏʊӄ/__main__.py
+++ ֆɦɨռɨɢǟʍɨ_ʀʏʊӄ/__main__.py
@@ -1,1 +1,96 @@
+"""
+     /  \        /  \        /  \        /  \        /  \        /  \
+__/        \__/        \__/        \__/        \__/        \__/       
+  \        /  \        /  \        /  \        /  \        /  \       
+     \__/        \__/        \__/        \__/        \__/        \__/
+            𝔻𝕠𝕟❜𝕥 𝕂𝕒𝕟𝕘 𝕋𝕙𝕖 ℝ𝕖𝕡𝕠 𝕨𝕚𝕥𝕙𝕠𝕦𝕥 𝕤𝕥𝕒𝕣𝕚𝕟𝕘 𝕒𝕟𝕕 𝕗𝕠𝕣𝕜𝕚𝕟𝕘...     
+                        🅼🅰🆂🆃🅴🆁🅼🅸🅽🅳🆅🆁🆃🆇    
+     /  \        /  \        /  \        /  \        /  \        /  \
+__/        \__/        \__/        \__/        \__/        \__/       
+  \        /  \        /  \        /  \        /  \        /  \       
+     \__/        \__/        \__/        \__/        \__/        \__/
+"""     
+import sys 
+import time
+import logging
+from pyrogram import Client as mapple, idle
+from ᴋɪʀᴀ_ʟɪɢʜᴛ.pyro_auth import Li
+from ɴᴏᴛᴇʙᴏᴏᴋ.design_i import *
+"""
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+"""
+API_ID = Li.API_ID
+API_HASH = Li.API_HASH
+SESSION_NAME = Li.SESSION_NAME
+"""
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+"""
+#enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("__ʀʏʊӄi__.txt"),
+    logging.StreamHandler()
+    ],
+    #level=logging.INFO,
+)
 
+"""
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+"""
+PLUGINS = dict(
+    root="ᴅᴇᴀᴛʜ_ʙᴏᴏᴋ"
+)
+"""
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+"""
+ʍǟֆȶɛʀʍɨռɖ = mapple(
+    session_name=SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=PLUGINS,
+    workers=20
+    )
+ʍǟֆȶɛʀʍɨռɖ.start()
+print("\n\n\n\I am Asking Permission from @vrtxmusic.Please Hold on!\n")
+time.sleep(5)
+print("Getting all files ready!")
+time.sleep(3)
+print("Syncing!")
+print("Done!....Moving on\n")
+time.sleep(1)
+print("Checking MODULE Files!")
+time.sleep(1)
+print("Syncing!")
+time.sleep(3)
+print("Done!....Let's Start in:\n")
+print("                         3")
+time.sleep(1)
+print("                         2")
+time.sleep(1)
+print("                         1")
+print('🍁🎷一═デ︻ ֆɦɨռɨɢǟʍɨ_Rʏʊӄ ︻デ═一\nONLINE🍁🎷\n')
+print(DES_ME)
+idle()
+ʍǟֆȶɛʀʍɨռɖ.stop()
+print("Cleaning Done..\n\n\nExiting bot in:\n")
+time.sleep(1)
+print("               1")
+time.sleep(1)
+print("               2")
+time.sleep(1)
+print("               3\n\n\n")
+print('🍁⚰️一═デ︻ ֆɦɨռɨɢǟʍɨ_Rʏʊӄ ︻デ═一\nOFFLINE ⚰️🍁\n')
+print(DED_ME)
+"""
+  \        /  \        /  \        /  \        /  \        /  \       
+     \__/        \__/        \__/        \__/        \__/        \__/
+     /  \        /  \        /  \        /  \        /  \        /  \
+               ☠爪闩丂ㄒ㠪尺爪工𝓝ᗪᐯ尺ㄒ乂☠
+__/        \__/        \__/        \__/        \__/        \__/       
+  \        /  \        /  \        /  \        /  \        /  \       
+     \__/        \__/        \__/        \__/        \__/        \__/
+""" 
