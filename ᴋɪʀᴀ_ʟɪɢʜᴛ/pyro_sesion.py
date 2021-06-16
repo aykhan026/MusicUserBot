7bf4bc2390fee946fbf677b1b8baec14a74fda34
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-06-16 20:15:15 +0400

    Add files via upload

--- ᴋɪʀᴀ_ʟɪɢʜᴛ/pyro_sesion.py
+++ ᴋɪʀᴀ_ʟɪɢʜᴛ/pyro_sesion.py
@@ -10,17 +10,40 @@ __/        \__/        \__/        \__/        \__/        \__/
   \        /  \        /  \        /  \        /  \        /  \       
      \__/        \__/        \__/        \__/        \__/        \__/
 """   
-import os
+
+import tgcrypto
+import asyncio
+from pyrogram import Client as dedsec
 """
  \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
  / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
 """
-class Li:
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
-    SESSION_NAME = os.environ["SESSION_NAME"]
-    DYNO_COMMAND = os.environ["DYNO_COMMAND"]  
-    WHITE_COMMAND = os.environ["WHITE_COMMAND"] 
+async def ryuk():
+    print("Enter your credentials of telegram below:")
+    api_id = int(input("API ID:- "))
+    print("Okay Saved in my archive.\nNow send me:")
+    api_hash = input("API HASH:- ")
+    async with dedsec(":memory:", api_id=api_id, api_hash=api_hash) as kryo:
+        await kryo.send_message(
+            "me",
+            "**一═デ︻ ֆɦɨռɨɢǟʍɨ Rʏʊӄ ︻デ═一**:\n\n"
+            "from |=== ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ -_-====|\n"
+            "For any support ask me in here @vrtxmusic"
+            f"`{await kryo.export_session_string()}`"
+        )
+        print(
+            "**一═デ︻ ֆɦɨռɨɢǟʍɨ Rʏʊӄ ︻デ═一**:\n\n"
+            "YEY! Check your saved messages in Telegram"
+            "from |=== ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ -_-====|\n"
+            "For any support ask me in here @vrtxmusic"
+        )
+"""
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+"""
+if __name__ == "__ryuk__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(ryuk())    
 """
 \__/        \__/        \__/        \__/        \__/  
 /  \        /  \        /  \        /  \        /  \ 
